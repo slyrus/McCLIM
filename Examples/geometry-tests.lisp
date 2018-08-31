4809ce2161c1164b2e4fb1b7fa98e4bf7f0517e9
Cyrus Harmon <cyrus@bobobeach.com>
2018-08-31 11:04:55 -0700

    add a polygon to the simple pane drawing routine

--- Examples/geometry-tests.lisp
+++ Examples/geometry-tests.lisp
@@ -14,8 +14,13 @@
   (declare (ignore region))
   (multiple-value-bind (x1 y1 x2 y2)
       (bounding-rectangle* (sheet-region pane))
-    (draw-rectangle* pane (+ x1 10) (+ y1 10) (- x2 10) (- y2 10) :line-thickness 5 :ink +red+ :filled nil)
-    (draw-rectangle* pane (+ x1 15) (+ y1 15) (- x2 15) (- y2 15) :line-thickness 5 :ink +light-blue+ :filled t)
+    (draw-rectangle* pane (+ x1 10) (+ y1 10) (- x2 10) (- y2 10)
+                     :line-thickness 5 :ink +red+ :filled nil)
+    (draw-rectangle* pane (+ x1 15) (+ y1 15) (- x2 15) (- y2 15)
+                     :line-thickness 5 :ink +light-blue+ :filled t)
+    (draw-polygon* pane
+                   '(100 100 200 100 180 140 130 130 100 100)
+                   :line-thickness 5 :ink +brown+ :filled t :filled nil)
     (draw-text* pane (format nil "sheet-region: (~D ~D ~D ~D)" x1 y1 x2 y2) 30 30)))
 
 (define-application-frame simple-app () ()
