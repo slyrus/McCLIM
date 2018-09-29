5fff3c6ded41f7823279b8405518b8e650ff0198
Cyrus Harmon <cyrus@bobobeach.com>
2018-09-29 13:23:43 -0700

    call standard-rectangle print-unreadable-object with :idenitity nil

--- Core/clim-basic/regions.lisp
+++ Core/clim-basic/regions.lisp
@@ -2689,7 +2689,7 @@
 ;;;
 
 (defmethod print-object ((self standard-rectangle) stream)
-  (print-unreadable-object (self stream :type t :identity t)
+  (print-unreadable-object (self stream :type t :identity nil)
     (with-standard-rectangle (x1 y1 x2 y2)
       self
       (format stream "X ~S:~S Y ~S:~S" x1 x2 y1 y2))))
